71614cebe91b3283e1f66f2201d565864077a6d8
Randy Oest <oest@amazingrando.com>
2024-07-30 16:54:32 -0400

    tracking plays

--- src/components/player/PlayButton.jsx
+++ src/components/player/PlayButton.jsx
@@ -1,5 +1,6 @@
 import { PauseIcon } from '@/components/PauseIcon'
 import { PlayIcon } from '@/components/PlayIcon'
+import * as Fathom from 'fathom-client';
 
 export function PlayButton({ player }) {
   let Icon = player.playing ? PauseIcon : PlayIcon
@@ -8,7 +9,12 @@ export function PlayButton({ player }) {
     <button
       type="button"
       className="group relative flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-kitchensKelly-700 hover:bg-kitchensKelly-900 focus:outline-none focus:ring-2 focus:ring-kitchensKelly-700 focus:ring-offset-2 md:h-14 md:w-14"
-      onClick={() => player.toggle()}
+      onClick={() => {
+        player.toggle();
+        if (!player.playing) {
+          Fathom.trackEvent(`Playing: ${player.episode.title}`);
+        }
+      }}
       aria-label={player.playing ? 'Pause' : 'Play'}
     >
       <div className="absolute -inset-3 md:hidden" />
